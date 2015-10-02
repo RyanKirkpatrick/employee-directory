197b4a35de435395abe81b309d999b7679d0fcfc
matt copeland <mattcopeland@local>
2015-10-01 21:25:13 -0400

    Allow for multiple first and last names in query string

--- public/app/main/main.controller.js
+++ public/app/main/main.controller.js
@@ -20,18 +20,47 @@
 			var selectedEmployees = employees.filter(function (employee) {
 				var match = false;
 				if ($stateParams.firstname) {
-					if (employee.firstName.toLocaleLowerCase() === $stateParams.firstname.toLowerCase()) {
-						match = true;
-					} else {
-						return false;
-					}
-				}
-				if ($stateParams.lastname) {
-					if (employee.lastName.toLocaleLowerCase() === $stateParams.lastname.toLowerCase()) {
-						match = true;
-					} else {
-						return false;
-					}
+					// Split first name query on comma
+					var firstnames = $stateParams.firstname.split(',');
+					// Loop over each first name
+					angular.forEach(firstnames, function (firstname) {
+						// If the employee's first name is part of the query check the last names
+						if (employee.firstName.toLocaleLowerCase() === firstname.toLowerCase()) {
+							// If last names are on the query string
+							if ($stateParams.lastname) {
+								// Split last name query on comma
+								var lastnames = $stateParams.lastname.split(',');
+								// Loop over all the last names
+								angular.forEach(lastnames, function (lastname) {
+									// If the employee's last name is part of the query string we have match
+									if (employee.lastName.toLocaleLowerCase() === lastname.toLowerCase()) {
+										match = true;
+									} else {
+										return false;
+									}
+								});
+							// No last name query so only worry about the first name
+							} else {
+								match = true;
+							}
+						// This employee's first name is not a match
+						} else {
+							return false;
+						}
+					});
+				// No first name query so only worry about the last name
+				} else if ($stateParams.lastname) {
+					var lastnames = $stateParams.lastname.split(',');
+					// Loop over all the last names
+					angular.forEach(lastnames, function (lastname) {
+						// If the employee's last name is part of the query string we have match
+						if (employee.lastName.toLocaleLowerCase() === lastname.toLowerCase()) {
+							match = true;
+						// This employee's last name is not a match
+						} else {
+							return false;
+						}
+					});
 				}
 				return match;
 			});
