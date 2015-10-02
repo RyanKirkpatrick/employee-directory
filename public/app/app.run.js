741d06a01b9789bc0f7c911a28dc84e31aaf0785
matt copeland <mattcopeland@local>
2015-10-01 21:51:18 -0400

    Clear state params on state change success

--- public/app/app.run.js
+++ public/app/app.run.js
@@ -2,9 +2,9 @@
 	'use strict';
 	angular.module('app').run(appRun);
 
-	appRun.$inject = ['$rootScope', '$state', '$document'];
+	appRun.$inject = ['$rootScope', '$state', '$stateParams', '$document'];
 
-	function appRun($rootScope, $state, $document) {
+	function appRun($rootScope, $state, $stateParams, $document) {
 		$rootScope.$on('$stateChangeError', function (event, toState, toParams, fromState, fromParams, error) {
 			if (error === 'not authorized') {
 				$state.go('login');
@@ -13,6 +13,8 @@
 
 		$rootScope.$on('$stateChangeSuccess', function() {
 			$document[0].body.scrollTop = $document[0].documentElement.scrollTop = 0;
+			$stateParams.firstname = null;
+			$stateParams.lastname = null;
 		});
 	}
 })();
