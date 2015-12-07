bf4690908f32022154c4db95fc37ba64910a045a
matt copeland <mattcopeland@local>
2015-12-06 22:24:04 -0500

    Add ng-focus-if directive

--- public/app/app.module.js
+++ public/app/app.module.js
@@ -1,6 +1,6 @@
 (function () {
 	'use strict';
-	angular.module('app', ['ngResource', 'ui.router', 'ngFileUpload', 'angular.filter', 'ngAnimate', 'duScroll', 'angular.vertilize', 'angularUtils.directives.dirPagination']);
+	angular.module('app', ['ngResource', 'ui.router', 'ngFileUpload', 'angular.filter', 'ngAnimate', 'duScroll', 'angular.vertilize', 'angularUtils.directives.dirPagination', 'focus-if']);
 
 	angular.module('app').constant('_', window._);
 })();
