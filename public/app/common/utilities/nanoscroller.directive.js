cea5662f765f0cc3bcbe3865d5a4ff93dbd699b4
matt copeland <mattcopeland@local>
2015-12-11 12:58:19 -0500

    Fix some nanoscroller display issues

--- public/app/common/utilities/nanoscroller.directive.js
+++ public/app/common/utilities/nanoscroller.directive.js
@@ -16,8 +16,11 @@
 		return directive;
 
 		function linkFunc(scope, el, attrs) {
+			// Do this right away so scrollbars don't display in IE and Firefox
+			angular.element('.nano').nanoScroller({preventPageScrolling: true});
+			// Do this again after a few seconds to wait for the model to populate
 			$timeout(function () {
-				angular.element('.nano').nanoScroller({preventPageScrolling: true});
+				angular.element('.nano').nanoScroller();
 			}, 3000);
 		}
 	}
