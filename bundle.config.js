34d092c996b66601a170ee4c62f70741b2bff878
Matt Copeland <mattcopeland@gmail.com>
2016-01-08 16:33:50 -0500

    Bundling javascript

---
+++ bundle.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+	bundle: {
+		main: {
+			scripts: [
+				'./public/app/**/*.js'
+			]
+		},
+		vendor: {
+			scripts: [
+				'./bower_components/jquery/dist/jquery.min.js',
+				'./bower_components/toastr/toastr.min.js',
+				'./bower_components/angular/angular.min.js',
+				'./bower_components/angular-resource/angular-resource.min.js',
+				'./bower_components/angular-ui-router/release/angular-ui-router.min.js',
+				'./bower_components/angular-animate/angular-animate.min.js',
+				'./bower_components/ng-file-upload/ng-file-upload.min.js',
+				'./bower_components/angular-filter/dist/angular-filter.min.js',
+				'./bower_components/angular-scroll/angular-scroll.min.js',
+				'./bower_components/angular-vertilize/angular-vertilize.js',
+				'./bower_components/nanoscroller/bin/javascripts/jquery.nanoscroller.min.js',
+				'./bower_components/angularUtils-pagination/dirPagination.js',
+				'./bower_components/lodash/lodash.min.js',
+				'./bower_components/ng-focus-if/focusIf.min.js'
+			]
+		}
+	}
+};
---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+var gulp = require('gulp'),
+	bundle = require('gulp-bundle-assets');
+
+gulp.task('bundle', function () {
+	return gulp.src('./bundle.config.js')
+		.pipe(bundle())
+		.pipe(bundle.results('./'))
+		.pipe(gulp.dest('./public/build-assets'));
+});
