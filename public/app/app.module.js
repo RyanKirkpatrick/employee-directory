568f103ce2397d75a98227e5a767932bf3d1cabc
Matt Copeland <mattcopeland@gmail.com>
2016-03-06 21:42:09 -0500

    Remove Angular Material Use native date input for birthday

--- public/app/app.module.js
+++ public/app/app.module.js
@@ -1,6 +1,6 @@
 (function () {
 	'use strict';
-	angular.module('app', ['ngResource', 'ui.router', 'ngFileUpload', 'angular.filter', 'ngAnimate', 'duScroll', 'angular.vertilize', 'angularUtils.directives.dirPagination', 'focus-if', 'ngMaterial']);
+	angular.module('app', ['ngResource', 'ui.router', 'ngFileUpload', 'angular.filter', 'ngAnimate', 'duScroll', 'angular.vertilize', 'angularUtils.directives.dirPagination', 'focus-if']);
 
 	angular.module('app').constant('_', window._).constant('moment', window.moment);
 })();
